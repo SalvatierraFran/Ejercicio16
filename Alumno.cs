ae37a919d652cd2d48ae1c1f373012a4900ad2be
SalvatierraFran <franco14_93@hotmail.com>
2016-03-31 00:35:59 -0300

    Se creó la clase Alumno que todavía falta desarrollar.

---
+++ Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio16
+{
+    class Alumno
+    {
+        private byte _nota1;
+        private byte _nota2;
+        private float _notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public Alumno(string Nombre, string Apellido, int Legajo)
+        {
+            this.nombre = Nombre;
+            this.apellido = Apellido;
+            this.legajo = Legajo;
+        }
+
+        public static void CalcularFinal()
+        { }
+        
+        public static void Estudiar(byte notaUno, byte notaDos)
+        {
+            
+        }
+        
+        public static void Mostrar()
+        { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno PrimerAlumno = new Alumno("Franco", "Salvatierra", 1111);
+            Alumno SegundoAlumno = new Alumno("Angel", "Rozas", 2222);
+            Alumno TercerAlumno = new Alumno("Juan", "Cruz", 3333);
+        }
+    }
+}
